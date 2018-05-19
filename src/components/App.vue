f35b9c04eb42ad88166d41dde8775bbcc263a9f3
fzuccoli <fernando.zuccoli@polishop.com.br>
2018-05-18 21:19:37 -0300

    Initial Project

---
+++ src/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+<h1>{{aa}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Pinball',
+  data () {
+    return {
+      aa: 'Test'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
